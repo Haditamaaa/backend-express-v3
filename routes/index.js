924c825805b293374a97e52fb4a1e55df1d6a429
Surya Haditama <110581874+Haditamaaa@users.noreply.github.com>
2025-05-22 16:46:02 +0700

    firstcom

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+//import express
+const express = require("express");
+
+//init express router
+const router = express.Router();
+
+//import verifyToken
+const verifyToken = require("../middlewares/auth");
+
+//import register controller
+const registerController = require("../controllers/RegisterController");
+
+//import login controller
+const loginController = require("../controllers/LoginControllery");
+
+//import user controller
+const userController = require("../controllers/UserController");
+
+//import validate register and login
+const { validateRegister, validateLogin } = require("../utils/validators/auth");
+
+//import validate user
+const { validateUser } = require("../utils/validators/user");
+
+//define route for register
+router.post("/register", validateRegister, registerController.register);
+
+//define route for login
+router.post("/login", validateLogin, loginController.login);
+
+//define route for user
+router.get("/admin/users", verifyToken, userController.findUser);
+
+//define route for create
+router.post("/admin/users", verifyToken, validateUser, userController.createUser);
+
+//define route for user by id
+router.post("/admin/users/:id", verifyToken, validateUser, userController.findUserById);
+
+//define route for user update
+router.put("/admin/users/:id", verifyToken, validateUser, userController.updateUser);
+
+//define route for user delete
+router.delete("/admin/users/:id", verifyToken, userController.deleteUser);
+
+//export router
+module.exports = router;
